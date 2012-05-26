c5dbd6b0509843432aa4cebec484749e52135276
Webfaction Server <alex@weknowhtml.com>
2012-05-25 21:49:09 -0500

    add mp3s

--- apt.php
+++ apt.php
@@ -5,6 +5,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
 <Response>
   <Gather action="http://phone.indyhall.org/gather.php" method="POST">
-    <Say>If you're a guest or visitor, please press 0. Otherwise, enter your 4 digit code now.</Say>
+    <Play>http://phone.indyhall.org/voice/Welcome4.mp3</Play>
+    <Play>http://phone.indyhall.org/voice/Guest4.mp3</Play>
   </Gather>
 </Response>
--- gather.php
+++ gather.php
@@ -5,14 +5,20 @@ $members=array(
   ,"2345"=>"Adam Teterus"
   ,"3456"=>"Geoff DiMasi"
   ,"9999"=>"Johnny Bilotta"
+  ,"9876"=>"Parker Whitney"
 );
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 if(array_key_exists($_REQUEST['Digits'], $members)){
   ?>
   <Response>
-      <Say>Hello, <?php echo $members[$_REQUEST['Digits']] ?></Say>
-      <Play>http://idisk.s3.amazonaws.com/tmp/9.wav</Play>
+    <?  if($_REQUEST['Digits'] == '9876'){
+          ?>
+          <Play>http://phone.indyhall.org/voice/Parker1.mp3</Play>
+          <?
+        }
+    ?>
+    <Play>http://idisk.s3.amazonaws.com/tmp/9.wav</Play>
   </Response>
   <?php
   die;
@@ -20,7 +26,7 @@ if(array_key_exists($_REQUEST['Digits'], $members)){
 elseif($_REQUEST['Digits'] == '0'){
   ?>
   <Response>
-    <Say>Connecting you to Adam</Say>
+    <Play>http://phone.indyhall.org/voice/FrontDesk1.mp3</Play>
     <Dial>267-702-4865</Dial>
   </Response>
   <?php
@@ -30,7 +36,8 @@ else{
 ?>
   <Response>
     <Gather action="http://phone.indyhall.org/gather.php" method="POST">
-      <Say>You did not make a selection or your code is invalid. If you're a guest or visitor, please press 0. Otherwise, enter your 4 digit code now.</Say>
+      <Play>http://phone.indyhall.org/voice/Invalid3.mp3</Play>
+      <Play>http://phone.indyhall.org/voice/Guest4.mp3</Play>
     </Gather>
   </Response>
 <?php
