8e37d9c9a562770357eee9380334978d511ae3c9
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-06-07 17:42:08 -0400

    Translated into a flask app

---
+++ app.py
@@ -0,0 +1,75 @@
+"""
+Flask Documentation:     http://flask.pocoo.org/docs/
+
+This file creates your application.
+"""
+
+import os
+import sys
+
+# Make sure this file's path is in the PYTHONPATH (for utils and settings).
+sys.path.append(os.path.dirname(__file__))
+
+import utils
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Try to load settings from the 'settings.py' file.  If the module does not
+# exist, try loading from environment variables.
+#
+# Environment:
+# -----------
+# * SECRET_KEY
+#   An arbitrary string
+#
+# * MEMBER_DB_HOST (optional)
+#   In the form 'http://www.mycouchserver.com:1234/'.
+#
+# * MEMBER_DB_NAME
+#   The name of the specific member database
+
+try:
+    import settings
+    app.config.from_object(settings)
+except ImportError:
+    app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+    app.config['MEMBER_DB_HOST'] = os.environ.get('MEMBER_DB_HOST')
+    app.config['MEMBER_DB_NAME'] = os.environ['MEMBER_DB_NAME']
+
+
+###
+# Routing for your application.
+###
+
+@app.route('/first_contact')
+def first_contact():
+    r = utils.Responder(request.url_root)
+    r.redirect_to_authentication("voice/Welcome4.mp3")
+
+    return (str(r), 200, {'Content-type': 'text/xml'})
+
+
+@app.route('/authenticate_member')
+def authenticate_member():
+    digits = request.values['Digits']
+    r = utils.Responder(request.url_root)
+
+    if digits == '0':
+        r.redirect_to_front_desk()
+
+    else:
+        db = utils.MemberStore(app.config)
+        member = db.get_member_by_code(digits)
+        r.authenticate_member(member)
+
+    return (str(r), 200, {'Content-type': 'text/xml'})
+
+
+@app.route('/voice/<file_name>.mp3')
+def send_voice_file(file_name):
+    return app.send_static_file('voice/' + file_name + '.mp3')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ utils.py
@@ -0,0 +1,65 @@
+import couchdb
+import twilio.twiml
+
+class MemberStore (object):
+    def __init__(self, config):
+        db_host = config.get('MEMBER_DB_HOST')
+        db_name = config.get('MEMBER_DB_NAME')
+        db_args = dict(url=db_host) if db_host else {}
+        self.db = couchdb.Server(**db_args)[db_name]
+
+    def get_member_by_code(self, code):
+        """
+        Get an array representing the member from storage.  Return null if no
+        member with the given code could be found.
+        """
+        try:
+            member_data = self.db[code]
+            return Member(self, member_data)
+        except couchdb.ResourceNotFound:
+            return None
+        except Exception, e:
+            raise Exception("Unable to get {0} : {1}".format(code, e))
+
+
+class Member (object):
+    def __init__(self, store, data):
+        self.store = store
+        self.data = data
+
+    @property
+    def tone(self):
+        """
+        Get the tone from the given member array, or fall back to some default.
+        """
+        if 'tone' in self.data:
+            return self.data['tone']
+        return "http://idisk.s3.amazonaws.com/tmp/9.wav";
+
+
+class Responder (object):
+    """
+    Take care of all the Twilio stuff.
+
+    """
+    def __init__(self, host):
+        self.r = twilio.twiml.Response()
+        self.host = host
+
+    def redirect_to_front_desk(self):
+        self.r.play(self.host + "voice/FrontDesk1.mp3")
+        self.r.dial("267-702-4865")
+
+    def authenticate_member(self, member):
+        if member is not None:
+            self.r.play(member.tone)
+        else:
+            self.redirect_to_authentication("voice/Invalid3.mp3")
+
+    def redirect_to_authentication(message):
+        with self.r.gather(action=self.host + "authenticate_member") as auth:
+            auth.play(self.host + message)
+            auth.play(self.host + "voice/Guest4.mp3")
+
+    def __str__(self):
+        return self.r.toxml()
