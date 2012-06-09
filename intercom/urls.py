ba75400beacc35af37e5d72fecdcf67faaa0da47
Mjumbe Wawatu Poe <mjumbewu@gmail.com>
2012-06-09 15:23:43 -0400

    Add the voice static file urls

--- intercom/urls.py
+++ intercom/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
-from . import views
+from django.contrib.staticfiles.views import serve
+from .views import entry_point, authenticate_member
 
 urlpatterns = patterns('',
-    url(r'^first_contact$', views.entry_point, name='entry_point'),
-    url(r'^authenticate_member$', views.authenticate_member, name='authenticate_member'),
+    url(r'^first_contact$', entry_point, name='entry_point'),
+    url(r'^authenticate_member$', authenticate_member, name='authenticate_member'),
+    url(r'^(?P<path>voice/.*)$', serve),
 )
--- src/urls.py
+++ src/urls.py
@@ -5,8 +5,8 @@ import intercom.urls
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^', include(intercom.urls)),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(intercom.urls)),
 )
 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
